5a8e3bdd488c05d1c9696cc2a024742c3f31720c
sylvester <sylvesterkendricks@gmail.com>
2017-04-05 22:01:36 -0500

    Initial Commit

---
+++ SimpleShell_0.1.0/SimpleShell_0.1.0/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+// I have some simple functions that works perfectly well, but what
+// you will need to do is write a static function and then add it with
+// the shell.AddCommand() and the first parameter is the function name
+// in other words how it will be invoked or is recognized and the next parameter
+// is the function which must take in a type of argument
+namespace SimpleShell_0._1._0
+{
+    class Program
+    {
+        private static string _directory = Directory.GetCurrentDirectory();
+
+        // Sample functions
+        // If the function returns 0 then the program will end
+        public static int lsh_listFiles(string[] arguments)
+        {
+            var files = Directory.GetFiles(_directory);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+
+            return 1;
+        }
+
+        public static int lsh_exit(string[] arguments)
+        {
+            return 0;
+        }
+
+        public static int lsh_CD(string[] arguments)
+        {
+            if (Directory.Exists(arguments[0]))
+            {
+                _directory = arguments[0];
+            }
+            else
+            {
+                Console.WriteLine("invalid directory");
+            }
+
+            return 1;
+        }
+
+        public static int lsh_Clear(string[] arguments)
+        {
+            Console.Clear();
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Shell shell = new Shell();
+            shell.AddCommand("ls", lsh_listFiles);
+            shell.AddCommand("exit", lsh_exit);
+            shell.AddCommand("cd", lsh_CD);
+            shell.AddCommand("clear", lsh_Clear);
+
+            shell.Init();
+        }
+    }
+}
---
+++ SimpleShell_0.1.0/SimpleShell_0.1.0/Shell.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleShell_0._1._0
+{
+    // Adds commands based on functions rather than objects
+    // TODO: Go for an object heirarchy for this setup
+    public class Shell
+    {
+        public static int RUN_CODE = 1;
+        public static int EXIT_CODE = 0;
+
+        private static char[] delimeters = {'\n', '\t', ' '};
+        // Holds all of the functions for the shell
+        private Dictionary<string, Func<string[], int>> Aliases = new Dictionary<string, Func<string[], int>>();
+
+        // Add a new function to the shell
+        public void AddCommand(string alias, Func<string[], int> function)
+        {
+            try
+            {
+                Aliases.Add(alias, function);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        // Initialize shell
+        public void Init()
+        {
+            int status = RUN_CODE;
+            string line;
+            string[] arguments;
+
+            do
+            {
+                Console.Write("~$ ");
+                line = Console.ReadLine();
+                arguments = line.Split(delimeters);
+                status = Execute(arguments);
+
+            } while (status != EXIT_CODE);
+        }
+
+        // Execution code for functions
+        private int Execute(string[] arguments)
+        {
+            int status = RUN_CODE;
+
+            for (int i = 0; i < arguments.Length; i++)
+            {
+                if (Aliases.ContainsKey(arguments[i]))
+                {
+                    if (arguments.Length == 0)
+                    {
+                        status = Aliases[arguments[i]](null);
+                    }
+                    else
+                    {
+                        string[] args = arguments.Skip(1).ToArray();
+                        status = Aliases[arguments[i]](args);
+                    }
+                }
+            }
+
+            return status;
+        }
+    }
+}
